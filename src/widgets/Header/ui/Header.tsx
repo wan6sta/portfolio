3df2e18708b5a7f8010941c6bc27ee747ae3b4ed
wan6sta <wan6sta@gmail.com>
2023-01-11 02:57:05 +0300

    Add favicon, add header, nav

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,4 +1,12 @@
 import cls from './Header.module.scss'
+import {
+  ChatBubbleLeftIcon,
+  CommandLineIcon,
+  CubeIcon,
+  HomeIcon
+} from '@heroicons/react/24/outline'
+import { ReactComponent as PortfolioIcon } from 'shared/assets/icons/portfolioIcon.svg'
+import { EnvelopeIcon, PhoneArrowUpRightIcon } from '@heroicons/react/24/solid'
 
 export const Header = () => {
   return (
@@ -8,9 +16,36 @@ export const Header = () => {
           <div className={cls.img}></div>
           <h5>Ivan Korablin</h5>
         </div>
-        <nav className={cls.links}></nav>
+        <nav className={cls.links}>
+          <a>
+            <HomeIcon />
+            <span>Home</span>
+          </a>
+          <a>
+            <CommandLineIcon />
+            <span>Skills</span>
+          </a>
+          <a>
+            <CubeIcon />
+            <span>Portfolio</span>
+          </a>
+          <a>
+            <ChatBubbleLeftIcon />
+            <span>Contact me</span>
+          </a>
+        </nav>
       </div>
-      <div className={cls.socials}></div>
+      <div className={cls.socials}>
+        <a href='tel:+79814412617' className={cls.phone}>
+          <PhoneArrowUpRightIcon />
+        </a>
+        <a href='mailto:wan6sta@gmail.com' className={cls.gmail}>
+          <EnvelopeIcon />
+        </a>
+        <a className={cls.cv}>
+          <PortfolioIcon />
+        </a>
+      </div>
     </header>
   )
 }
