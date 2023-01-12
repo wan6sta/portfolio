47ba9b526ea3f027a2074d9760a6569e7765b4e4
wan6sta <wan6sta@gmail.com>
2023-01-12 11:08:45 +0300

    Add Particles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,19 @@
-import { Header } from 'widgets/Header'
-import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
 import { useTheme } from './providers/ThemeProvider'
 import { cn } from 'shared/lib/cn/cn'
 import { Home } from 'widgets/Main'
 import { Skills } from 'widgets/Skills'
 import { Portfolio } from 'widgets/Portfolio'
 import { Contact } from 'widgets/Contact'
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
+import { Header } from 'widgets/Header'
+import { Particle } from '../widgets/Particles'
 
 export const App = () => {
   const { theme } = useTheme()
 
   return (
     <div className={cn('app', {}, [theme])}>
+      <Particle />
       <Header />
       <main className='main'>
         <Home />
